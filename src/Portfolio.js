b783440173a6a43655696ed497566d0ef3e17513
Alexey <avdeev.hexer@yandex.ru>
2023-05-01 18:45:13 +0300

    modified:   src/App.js 	new file:   src/Portfolio.js 	new file:   src/ProjectList.js 	new file:   src/Toolbar.js

---
+++ src/Portfolio.js
@@ -0,0 +1,54 @@
+import {Component} from "react";
+import { useState } from "react";
+import ProjectList from "./ProjectList";
+import Toolbar from "./Toolbar";
+
+class Portfolio extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render () {
+        const filterBtnList = ['All', 'Websites', 'Flayers', 'Business Cards'];
+        const projectList = {projectList};
+        const [filteredListProjects, setFilteredListProjects] = useState(projectList);
+        const [selected, setSelected] = useState('All');
+        const filter = (e) => {
+            const buttons = document.querySelectorAll('button');
+            buttons.forEach((item) => item.classList.remove('active'));
+            
+            e.target.classList.add('active');
+        
+            const select = e.target.textContent;
+            
+            if (select === 'All') {
+              setFilteredListProjects(projectList);
+            } else {
+              setFilteredListProjects(
+                projectList.filter(item => item.category === select)
+              );
+            }
+        
+            setSelected(select);
+        
+            console.log(select);
+        }
+
+        return (
+            <div>
+                <Toolbar 
+                    filters={filterBtnList}
+                    selected={selected}
+                    onSelectFilter={filter}
+                />
+
+                <div>
+                    <ProjectList projects={filteredListProjects} /> 
+                </div>
+            </div>
+        );
+
+    }
+}
+
+export default Portfolio;
